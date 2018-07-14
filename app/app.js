c423c03e84d6c4d5fd9619e49b881a068f914ceb
Majid Ansari <33059093+Majid023@users.noreply.github.com>
2018-07-14 16:53:19 +0530

    init nodejs

---
+++ app/app.js
@@ -0,0 +1,22 @@
+var mongoDB = require( "mongodb" );
+var express = require( "express" );
+var reload = require( "reload" );
+var app = express();
+
+
+
+app.set("port", process.env.PORT || 4000);
+app.set( "view engine", "ejs" );
+app.set( "views", "app/views" );
+
+
+app.use( express.static( "app/public" ));
+app.use( require( "./routers/index" ));
+
+var server = app.listen(app.get("port"));
+
+console.log("server is started at port : " + app.get("port"));
+
+reload( server, app );
+
+
